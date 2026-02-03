a7e217bfa4a2a8e2592a04a01ade08a4204c98db
Paul Hoerenz <development@hoerenz.com>
2026-02-03 10:27:34 +0100

    feat: implement scan results for mcp-context-protector

--- src/main.py
+++ src/main.py
@@ -1,16 +1,83 @@
 import asyncio
 import logging
+from functools import reduce
 
+from dataset import load_mcptox
 from tools.mcp_context_protector import MCPContextProtector
+from tools.mock_server import MockServer
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("benchmark")
 
 
-async def main():
+def generate_tool_call(name: str, args: dict) -> dict:
+    return {
+        "mcp_request": {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "tools/call",
+            "params": {
+                "name": name,
+                "arguments": args,
+            },
+        },
+    }
+
+
+async def run_mock_server():
+    server = None
+
+    try:
+        server_data = load_mcptox()[0]
+
+        server = MockServer()
+        await server.start(instructions=server_data.instruction)
+
+        server.add_tool("print", "print", "Print text", lambda text: print(text))
+
+        benign_callback = lambda x: "benign tool response"  # pylint: disable=C3001
+        malicious_callback = lambda: "malicious tool response"  # pylint: disable=C3001
+        # malicious_callback = lambda: "Nothing to see here"  # pylint: disable=C3001
+
+        for tool in server_data.tools:
+            server.add_tool(
+                name=tool.name,
+                title=tool.name,
+                description=tool.description,
+                callback=malicious_callback if tool.malicious else benign_callback,
+            )
+
+        while True:
+            await asyncio.sleep(10)
+
+    except asyncio.CancelledError:
+        if server:
+            await server.stop()
+
+
+async def test_mcpcp():
     mcp_context_protector = MCPContextProtector()
     await mcp_context_protector.initialize()
+
+    tool_call_results = []
+    for _ in range(10):
+        result = await mcp_context_protector.evaluate_request(
+            generate_tool_call("sky", {})
+        )
+        tool_call_results.append(result)
+
+    avg_latency = reduce(lambda sum, r: sum + r.latency_ms, tool_call_results, 0) / 10
+    logger.debug("avg mcp-context-protector latency: %d ms", avg_latency)
+
     await mcp_context_protector.close()
+
+
+async def main():
+    server_task = asyncio.create_task(run_mock_server())
+
+    mcpcp_task = asyncio.create_task(test_mcpcp())
+    await mcpcp_task
+    server_task.cancel()
 
 
 if __name__ == "__main__":
--- src/tools/mcp_context_protector.py
+++ src/tools/mcp_context_protector.py
@@ -1,19 +1,76 @@
+import json
 import logging
 import os
+import re
+from timeit import default_timer as timer
+from typing import Optional
 
 from dotenv import load_dotenv
 
 from defense_adapter import DefenseAdapter, ScanResult
 from mcp_session import create_stdio_session
-from tools.mock_server import MCP_SERVER_PORT, MockServer
+from tools.mock_server import MCP_SERVER_PORT
 
 load_dotenv()
 logger = logging.getLogger(__name__)
 
+QUARANTINE_PATTERN = re.compile(r"--review-quarantine --quarantine-id")
+REASON_PATTERN = re.compile(r"Alert explanation: ([\w ]+),")
+CONFIDENCE_PATTERN = re.compile(r"with a probability of (\d.\d+).")
+
+
+def _is_quarantine_response(text: str) -> bool:
+    """
+    Determine if the answer is a quarantine response
+    of mcp-context-protector
+
+    Args:
+        text: text from the tool response
+    """
+
+    return (
+        re.search(
+            QUARANTINE_PATTERN,
+            text,
+        )
+        is not None
+    )
+
+
+def _parse_reason(text: str) -> Optional[str]:
+    """
+    Parse the reason from a mcp-context-protector
+    quarantine response
+
+    Args:
+        text: text from the tool response
+    """
+
+    reason_match = re.search(REASON_PATTERN, text)
+    if reason_match:
+        return reason_match.group(1)
+
+    return None
+
+
+def _parse_confidence(text: str) -> Optional[float]:
+    """
+    Parse the confidence from a mcp-context-protector
+    quarantine response
+
+    Args:
+        text: text from the tool response
+    """
+
+    confidence_match = re.search(CONFIDENCE_PATTERN, text)
+    if confidence_match:
+        return float(confidence_match.group(1))
+
+    return None
+
 
 class MCPContextProtector(DefenseAdapter):
     def __init__(self):
-        self.mock_server = None
         self._session_cm = None
         self.session = None
 
@@ -21,23 +78,13 @@ class MCPContextProtector(DefenseAdapter):
         """
         Initialize the mcp-context-protector defense adapter.
         """
-        self.mock_server = MockServer()
-        await self.mock_server.start()
-
-        # mcp-context-protector requires at least one tool to start
-        self.mock_server.add_tool(
-            name="echo",
-            title="Print Tool",
-            description="Print input server-side",
-            callback=lambda x: print(x),
-        )
 
         executable = os.getenv("MCP_CONTEXT_PROTECTOR_EXECUTABLE")
         if not executable:
-            logger.fatal(
+            logger.error(
                 "Missing path to mcp-context-protector executable in environment variables"
             )
-            raise ValueError(
+            raise RuntimeError(
                 "MCP_CONTEXT_PROTECTOR_EXECUTABLE is not set in environment variables"
             )
 
@@ -52,14 +99,50 @@ class MCPContextProtector(DefenseAdapter):
         self._session_cm = create_stdio_session(command)
         self.session = await self._session_cm.__aenter__()  # pylint: disable=C2801
 
-    async def evaluate_request(self, request: dict):
+    async def evaluate_request(self, request: dict) -> ScanResult:
         """
         Evaluate an incoming request for potential threats.
 
         Args:
             request: The incoming request to be evaluated.
         """
-        return ScanResult(blocked=False)
+
+        if not self.session:
+            logger.error("No session to send request to. Did you call 'initialize'?")
+            raise RuntimeError("Missing mcp-context-protector session")
+
+        if request["mcp_request"]["method"] != "tools/call":
+            raise ValueError("mcp-context-protector adapter only supports tool/call")
+
+        start = timer()
+        result = await self.session.call_tool(
+            request["mcp_request"]["params"]["name"],
+            request["mcp_request"]["params"]["arguments"],
+        )
+        end = timer()
+
+        scan_result = ScanResult(
+            blocked=False, reason=None, confidence=None, latency_ms=(end - start) * 1000
+        )
+
+        if len(result.content) != 1 or result.content[0].type != "text":
+            return scan_result
+
+        json_body = json.loads(result.content[0].text)
+
+        if not isinstance(json_body, dict) or json_body.get("response", "") == "":
+            return scan_result
+
+        response = json_body["response"]
+
+        if not _is_quarantine_response(response):
+            return scan_result
+
+        scan_result.blocked = True
+        scan_result.reason = _parse_reason(response)
+        scan_result.confidence = _parse_confidence(response)
+
+        return scan_result
 
     async def close(self):
         """
@@ -69,6 +152,3 @@ class MCPContextProtector(DefenseAdapter):
             await self._session_cm.__aexit__(None, None, None)
             self._session_cm = None
         self.session = None
-
-        if self.mock_server:
-            await self.mock_server.stop()
