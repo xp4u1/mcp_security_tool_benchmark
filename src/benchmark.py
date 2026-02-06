4e8349bfd2780b7ed427b5c9d050339c4544977f
Paul Hoerenz <development@hoerenz.com>
2026-02-06 09:35:15 +0100

    feat: add proxy benchmark

---
+++ src/benchmark.py
@@ -0,0 +1,54 @@
+import logging
+
+from dataset import ServerData
+from proxy_adapter import ProxyAdapter, ScanResult
+from tools.mock_server import MockServer
+
+logger = logging.getLogger(__name__)
+
+
+def generate_tool_call(name: str, args: dict) -> dict:
+    return {
+        "mcp_request": {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "tools/call",
+            "params": {
+                "name": name,
+                "arguments": args,
+            },
+        },
+    }
+
+
+async def benchmark_proxy(
+    server_data: ServerData, proxy: ProxyAdapter
+) -> list[ScanResult]:
+    server = MockServer()
+    await server.start(instructions=server_data.instruction)
+
+    benign_callback = lambda x: "benign tool response"  # pylint: disable=C3001
+    # malicious_callback = lambda: "malicious tool response"  # pylint: disable=C3001
+
+    logger.debug("Register %d tools/prompts/resources", len(server_data.tools))
+    for tool in server_data.tools:
+        server.add_tool(
+            name=tool.name,
+            title=tool.name,
+            description=tool.description,
+            callback=benign_callback,
+        )
+        server.add_prompt(tool.name, tool.description)
+        server.add_resource(tool.name, tool.description)
+
+    await proxy.initialize()
+
+    tool_call_results = []
+    for tool in server_data.tools:
+        result = await proxy.evaluate_request(generate_tool_call(tool.name, {}))
+        tool_call_results.append(result)
+
+    await proxy.close()
+    await server.stop()
+
+    return tool_call_results
--- src/main.py
+++ src/main.py
@@ -1,85 +1,28 @@
 import asyncio
+import json
 import logging
-from functools import reduce
 
+from benchmark import benchmark_proxy
 from dataset import load_mcptox
 from tools.mcp_context_protector import MCPContextProtector
-from tools.mock_server import MockServer
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("benchmark")
 
 
-def generate_tool_call(name: str, args: dict) -> dict:
-    return {
-        "mcp_request": {
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "tools/call",
-            "params": {
-                "name": name,
-                "arguments": args,
-            },
-        },
-    }
-
-
-async def run_mock_server():
-    server = None
-
-    try:
-        server_data = load_mcptox()[0]
-
-        server = MockServer()
-        await server.start(instructions=server_data.instruction)
-
-        server.add_tool("print", "print", "Print text", lambda text: print(text))
-
-        benign_callback = lambda x: "benign tool response"  # pylint: disable=C3001
-        malicious_callback = lambda: "malicious tool response"  # pylint: disable=C3001
-        # malicious_callback = lambda: "Nothing to see here"  # pylint: disable=C3001
-
-        for tool in server_data.tools:
-            server.add_tool(
-                name=tool.name,
-                title=tool.name,
-                description=tool.description,
-                callback=malicious_callback if tool.malicious else benign_callback,
-            )
-            server.add_prompt(tool.name, tool.description)
-            server.add_resource(tool.name, tool.description)
-
-        while True:
-            await asyncio.sleep(10)
-
-    except asyncio.CancelledError:
-        if server:
-            await server.stop()
-
-
-async def test_mcpcp():
-    mcp_context_protector = MCPContextProtector()
-    await mcp_context_protector.initialize()
-
-    tool_call_results = []
-    for _ in range(10):
-        result = await mcp_context_protector.evaluate_request(
-            generate_tool_call("sky", {})
-        )
-        tool_call_results.append(result)
-
-    avg_latency = reduce(lambda sum, r: sum + r.latency_ms, tool_call_results, 0) / 10
-    logger.debug("avg mcp-context-protector latency: %d ms", avg_latency)
-
-    await mcp_context_protector.close()
-
-
 async def main():
-    server_task = asyncio.create_task(run_mock_server())
+    mcptox = load_mcptox()
+    mcp_context_protector = MCPContextProtector()
 
-    mcpcp_task = asyncio.create_task(test_mcpcp())
-    await mcpcp_task
-    server_task.cancel()
+    results = []
+    for server_data in mcptox:
+        logger.info("Benchmark '%s' server", server_data.name)
+        scan_result = await benchmark_proxy(server_data, mcp_context_protector)
+        for result in scan_result:
+            results.append(result.__dict__)
+
+    with open("results.json", "w", encoding="utf8") as file:
+        json.dump(results, file)
 
 
 if __name__ == "__main__":
